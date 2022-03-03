7742daf2c7d3cb3dac784f430a6795d95947bba1
pedro <joao.pedro@fasolti.com.br>
2022-03-03 18:36:38 -0300

    Criacao do projeto

---
+++ primeiro-programa-java/src/exec/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package exec;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		try {
+			System.out.println("hello world");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+}
