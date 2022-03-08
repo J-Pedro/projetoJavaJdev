15f25c462e11cec1313f2ab6d9772fbb56ed5185
pedro <joao.pedro@fasolti.com.br>
2022-03-08 15:56:29 -0300

    Alterando classe

---
+++ primeiro-programa-java/src/exec/Pessoa.java
@@ -0,0 +1,39 @@
+package exec;
+
+import java.util.Date;
+
+public class Pessoa {
+
+	private String nome;
+	private Date dataNasc;
+	private int cpf;
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+	
+	
+}
